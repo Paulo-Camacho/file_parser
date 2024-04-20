8d9efe77269d97d9b6e7f32916600c417a3d6318
Paulo-Camacho <anthropologyparliament@gmail.com>
2024-04-19 22:33:29 -0700

    Template 2

--- parser.py
+++ parser.py
@@ -16,8 +16,8 @@ def rename_files_to_convention(directory):
             os.rename(old_file_path, new_file_path)
             print(f"Renamed {filename} to {new_filename}")
         
-        # Check if the file name contains a hyphen
-        elif '-' in filename:
+        # Check if the file name doesn't follow the convention example-example
+        elif not filename.replace('-', '').isalnum() or '-' not in filename:
             # Construct the new file name with the naming convention example-example
             words = filename.split('-')
             new_filename = '-'.join([word.lower() for word in words])
